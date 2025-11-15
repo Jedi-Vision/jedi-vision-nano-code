13d7141f101e0adc754c65e8ef12df0846936a72
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-11-14 22:02:48 -0800

    Added data serialization to server test example. Change recv from string_recv to frame_recv to ensure that bytes are preserved.

--- tests/object_buffer/struct/server.c
+++ tests/object_buffer/struct/server.c
@@ -4,10 +4,7 @@
 
 #include <czmq.h>
 #include "../../../src/c_utils/c_utils.h"
-
-typedef struct {
-    int x;
-} Data;
+#include "../../../src/c_utils/parse_bytes.h"
 
 
 int main(int argc, char const *argv[])
@@ -23,14 +20,49 @@ int main(int argc, char const *argv[])
     assert(rc == 0);
 
     while (1) {
-        char *buffer = zstr_recv(responder);
+        zframe_t *frame = zframe_recv(responder);
+        if (!frame) {
+            // interrupted or error
+            break;
+        }
+        size_t len = zframe_size(frame);
+        u_int8_t *buffer = malloc(len);
+        if (!buffer) {
+            zframe_destroy(&frame);
+            fprintf(stderr, "malloc failed\n");
+            break;
+        }
+        memcpy(buffer, zframe_data(frame), len);
+        zframe_destroy(&frame);
 
-        // Copy bytes into struct
-        Data data;
-        memcpy(&data, buffer, sizeof(Data));
-        printf("x=%d\n", data.x);
+        ObjectRepData rep;
+        if (parse_object_rep(buffer, len, &rep) != 0) {
+            fprintf(stderr, "Failed to parse buffer.\n");
+            zstr_send(responder, "0");
+            zstr_free(&buffer);
+            return 1;
+        }
 
-        sleep (1);          //  Do some 'work'
+        printf("Parsed %d coords:\n", rep.num_coords);
+        for (int i = 0; i < rep.num_coords; ++i) {
+            ObjectXYCoordData *c = &rep.coords[i];
+            printf("  [%d] x=%f y=%f id=%d label='%d'\n", i, c->x, c->y, c->object_id, c->label);
+        }
+
+        if (rep.dtype) {
+            printf("Mask: dtype=%s ndim=%d bytes=%zu\n", rep.dtype, rep.ndim, rep.data_bytes);
+            if (rep.data && strcmp(rep.dtype, "float32") == 0 && rep.data_bytes >= 4) {
+                float v;
+                memcpy(&v, rep.data, sizeof(float));
+                printf("  first element (float32) = %f\n", v);
+            }
+        }
+
+        // free
+        free(rep.coords);
+        free(rep.shape);
+        free(rep.data);
+
         zstr_send(responder, "0");
         zstr_free(&buffer);
     }
