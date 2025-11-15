c5ca65702958ff1297bceafa665be3a975dc5825
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-11-14 22:01:16 -0800

    Add more arguments to main.py for Driver.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from jv import Driver
 
 driver = Driver(
     "yolo11",
-    "mps"
+    "mps",
+    # camera_index="examples/videos/sidewalk_pov.mp4",
+    object_buffer_size=1,
+    frame_buffer_size=1,
+    frame_skip=1
 )
 
 driver.run()
