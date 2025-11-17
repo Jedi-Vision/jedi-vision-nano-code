e19bcf9b3265cb3e9bb027eb9f852580bae9ce41
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-11-17 15:22:05 -0800

    Ensure that all frames are received if split up in transmission.

--- tests/object_buffer/struct/server.c
+++ tests/object_buffer/struct/server.c
@@ -20,20 +20,61 @@ int main(int argc, char const *argv[])
     assert(rc == 0);
 
     while (1) {
+        // Receive all frames if multipart message
+        size_t len = 0;
+        u_int8_t *buffer = NULL;
         zframe_t *frame = zframe_recv(responder);
-        if (!frame) {
+        zmsg_t *msg = NULL;
+        if (frame && zframe_more(frame)) {
+            // Multipart message: collect all frames
+            msg = zmsg_recv(responder);
+            zmsg_prepend(msg, &frame);
+            // Concatenate all frames into one buffer
+            size_t total_len = 0;
+            for (zframe_t *f = zmsg_first(msg); f; f = zmsg_next(msg)) {
+                total_len += zframe_size(f);
+            }
+            u_int8_t *buffer = malloc(total_len);
+            if (!buffer) {
+                zmsg_destroy(&msg);
+                fprintf(stderr, "malloc failed\n");
+                break;
+            }
+            size_t offset = 0;
+            for (zframe_t *f = zmsg_first(msg); f; f = zmsg_next(msg)) {
+                memcpy(buffer + offset, zframe_data(f), zframe_size(f));
+                offset += zframe_size(f);
+            }
+            zmsg_destroy(&msg);
+            len = total_len;
+        } else if (frame) {
+            // Single frame
+            len = zframe_size(frame);
+            buffer = malloc(len);
+            if (!buffer) {
+                zframe_destroy(&frame);
+                fprintf(stderr, "malloc failed\n");
+                break;
+            }
+            memcpy(buffer, zframe_data(frame), len);
+            zframe_destroy(&frame);
+        } else {
             // interrupted or error
             break;
         }
-        size_t len = zframe_size(frame);
-        u_int8_t *buffer = malloc(len);
-        if (!buffer) {
-            zframe_destroy(&frame);
-            fprintf(stderr, "malloc failed\n");
-            break;
-        }
-        memcpy(buffer, zframe_data(frame), len);
-        zframe_destroy(&frame);
+        // if (!frame) {
+        //     // interrupted or error
+        //     break;
+        // }
+        // len = zframe_size(frame);
+        // buffer = malloc(len);
+        // if (!buffer) {
+        //     zframe_destroy(&frame);
+        //     fprintf(stderr, "malloc failed\n");
+        //     break;
+        // }
+        // memcpy(buffer, zframe_data(frame), len);
+        // zframe_destroy(&frame);
 
         ObjectRepData rep;
         if (parse_object_rep(buffer, len, &rep) != 0) {
@@ -46,7 +87,11 @@ int main(int argc, char const *argv[])
         printf("Parsed %d coords:\n", rep.num_coords);
         for (int i = 0; i < rep.num_coords; ++i) {
             ObjectXYCoordData *c = &rep.coords[i];
-            printf("  [%d] x=%f y=%f id=%d label='%d'\n", i, c->x, c->y, c->object_id, c->label);
+
+            // Get the 1D indexing based on 2D shape of data
+            int width = rep.shape ? rep.shape[1] : 1;
+            int idx = (int)c->y * width + (int)c->x;
+            printf("  [%d] x=%f y=%f z=%f id=%d label='%d'\n", i, c->x, c->y, ((float*)rep.data)[idx], c->object_id, c->label);
         }
 
         if (rep.dtype) {
