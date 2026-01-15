95ce95b248d19472776f6dcfd5fbcd393deb81f1
Colin <19521833+colinpannikkat@users.noreply.github.com>
2026-01-15 00:24:57 -0800

    Add x11 forwarding stuff for Qt in container

--- start_container.sh
+++ start_container.sh
@@ -2,4 +2,6 @@ sudo docker run -it --rm \
     --runtime nvidia \
     --network host \
     -v $PWD:/workspace/jv \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
     jp61-orin-xformers
