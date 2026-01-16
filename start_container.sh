1be6ad66c27198406e0b1412654435a66fe40654
Colin <19521833+colinpannikkat@users.noreply.github.com>
2026-01-15 18:49:07 -0800

    Add volume link to ensure that X11/Qt works

--- start_container.sh
+++ start_container.sh
@@ -3,5 +3,6 @@ sudo docker run -it --rm \
     --network host \
     -v $PWD:/workspace/jv \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority:rw \
     -e DISPLAY=$DISPLAY \
     jp61-orin-xformers
