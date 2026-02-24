9a5712cfd50f166550168e6aae0faf5d1f64c363
Colin <19521833+colinpannikkat@users.noreply.github.com>
2026-02-23 18:43:58 -0800

    Update start container script to ensure that the Argus sockets are available through the docker container.

--- start_container.sh
+++ start_container.sh
@@ -1,9 +1,12 @@
 sudo docker run -it --rm \
     --runtime nvidia \
     --network host \
+    --privileged \  # lowkey sketchy but should be fine
     -v $PWD:/workspace/jv \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -v /tmp/:/tmp/ \
+    -v /tmp/argus_socket:/tmp/argus_socket \
     -v $HOME/.Xauthority:/root/.Xauthority:rw \
     -v /dev:/dev \
     -e DISPLAY=$DISPLAY \
+    -v /etc/machine-id:/etc/machine-id:ro \
     jp61-orin-xformers
