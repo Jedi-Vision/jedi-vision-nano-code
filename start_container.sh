ac4ac911f7219d1d72a26186e3cb36c0240d467e
Colin <19521833+colinpannikkat@users.noreply.github.com>
2026-04-26 20:35:56 -0700

    Update start container and Dockerfile

--- start_container.sh
+++ start_container.sh
@@ -20,6 +20,9 @@ DOCKER_ARGS=(
     -v /dev:/dev
     -e "DISPLAY=${DISPLAY:-}"
     -v /etc/machine-id:/etc/machine-id:ro
+    --device nvidia.com/gpu=all 
+    --device nvidia.com/pva=all 
+    -it
 )
 
 if [[ -z "${HOST_PULSE_SERVER}" && -S "${HOST_PULSE_DIR}/native" ]]; then
