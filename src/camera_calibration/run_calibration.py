89c61af80c0d050b736f3dfe6ce7f91dff1b8117
Colin <19521833+colinpannikkat@users.noreply.github.com>
2026-05-13 22:43:10 -0700

    Update run_calibration.py for new camera

--- src/camera_calibration/run_calibration.py
+++ src/camera_calibration/run_calibration.py
@@ -1,14 +1,15 @@
 from calibrate import stereoCalibrateCamera
 from jv.camera import FrameBuffer
+import cv2
 
 BOX_SIZE = 25  # mm
 GRID_SIZE = (10, 7)  # width times height, internal corners not true pattern size!
 
 gstreamer_kwargs = {
     "sensor_mode": 4,
-    "capture_width": 1280,
+    "capture_width": 2560,
     "capture_height": 720,
-    "display_width": 1280,
+    "display_width": 2560,
     "display_height": 720,
     "flip_method": 2,
 }
@@ -19,14 +20,15 @@ fb = FrameBuffer(
         frame_skip=0,
         frame_rate=60,
         binocular=True,
-        left_sensor_id=1,
-        right_sensor_id=0,
+        left_sensor_id=0,
+        right_sensor_id=None,
         gstreamer_kwargs=gstreamer_kwargs,
     )
 
 stereoCalibrateCamera(
     fb,
-    "imx219-83",
+    "elp",
     chessboard_box_size=BOX_SIZE,
-    chessboard_grid_size=GRID_SIZE
+    chessboard_grid_size=GRID_SIZE,
+    number_of_frames=100
 )
